89a624a6c2d7bc1d9fb2441093e1813819f30373
Louis Wang <phineco@gmail.com>
2018-03-06 09:42:35 +0800

    Add Login Form

---
+++ Source/Plugins/Login/Login/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Login
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "用户名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(52, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "密码";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUser.Location = new System.Drawing.Point(153, 50);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(191, 27);
+            this.tbUser.TabIndex = 2;
+            // 
+            // tbPass
+            // 
+            this.tbPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPass.Location = new System.Drawing.Point(153, 94);
+            this.tbPass.Name = "tbPass";
+            this.tbPass.PasswordChar = '*';
+            this.tbPass.Size = new System.Drawing.Size(191, 27);
+            this.tbPass.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(129, 174);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(99, 33);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "登录";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(245, 173);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(99, 33);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "退出";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(408, 225);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.tbPass);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "林地展示地理信息系统 - 登录";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.TextBox tbPass;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Source/Plugins/Login/Login/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using MySql.Data.MySqlClient;
+
+namespace Login
+{
+    public partial class Form_Main : Form
+    {
+        [DllImport("shell32.dll")]
+        public extern static IntPtr ShellExecute(IntPtr hwnd,
+                                                 string lpOperation,
+                                                 string lpFile,
+                                                 string lpParameters,
+                                                 string lpDirectory,
+                                                 int nShowCmd
+                                                );  
+
+        private const String GIS_EXE = "HydroDesktop_1_6_dev.exe";
+        private const String DEF_USER = "test";
+        private const String DEF_PASS = "test";
+
+
+        public Form_Main()
+        {
+            InitializeComponent();
+        }
+
+  
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (VerifyAccount(tbUser.Text, tbPass.Text))
+            {
+                ShellExecute(IntPtr.Zero, "open", GIS_EXE, "", "", 1);
+                Environment.Exit(0);
+            }
+            else
+            {
+                MessageBox.Show("请输入正确的用户名和密码");
+                return;
+            }
+        }
+
+        private bool VerifyAccount(String strUser, String strPass)
+        {
+            //if (strUser == DEF_USER && strPass == DEF_PASS)
+                //return true;
+
+            //return false;
+            return isLogin(strUser,strPass);
+        }
+
+        public static string connStr = "server=139.196.126.19;user id=;password=;database=dhdata;port=3306;";
+        public static MySqlConnection conn;
+        private bool isLogin(string userName, string password)
+        {
+            if (userName == null || password == null || userName.Length > 20 || password.Length > 20)
+            {
+                return false;
+            }
+            try
+            {
+                conn = new MySqlConnection(connStr);
+                string sqlStr = string.Format("select UserNum from users where UserName='{0}' and Password='{1}'", userName, password);
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(sqlStr, conn);
+                MySqlDataReader sdr = cmd.ExecuteReader();
+                if (sdr.Read())
+                {
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+
+
+
+            return false;
+        }
+    }
+}
