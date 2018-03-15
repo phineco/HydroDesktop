813b64b95a33b2d45687b984ff3c9a2739bd81e6
Louis Wang <phineco@gmail.com>
2018-03-15 13:03:05 +0800

    Add icon and modify text

--- Source/Plugins/Login/Login/Form1.Designer.cs
+++ Source/Plugins/Login/Login/Form1.Designer.cs
@@ -40,9 +40,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(52, 50);
+            this.label1.Location = new System.Drawing.Point(39, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.Size = new System.Drawing.Size(50, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "用户名";
             // 
@@ -50,35 +51,39 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(52, 94);
+            this.label2.Location = new System.Drawing.Point(39, 70);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.Size = new System.Drawing.Size(36, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "密码";
             // 
             // tbUser
             // 
             this.tbUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbUser.Location = new System.Drawing.Point(153, 50);
+            this.tbUser.Location = new System.Drawing.Point(115, 38);
+            this.tbUser.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbUser.Name = "tbUser";
-            this.tbUser.Size = new System.Drawing.Size(191, 27);
+            this.tbUser.Size = new System.Drawing.Size(144, 23);
             this.tbUser.TabIndex = 2;
             // 
             // tbPass
             // 
             this.tbPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbPass.Location = new System.Drawing.Point(153, 94);
+            this.tbPass.Location = new System.Drawing.Point(115, 70);
+            this.tbPass.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbPass.Name = "tbPass";
             this.tbPass.PasswordChar = '*';
-            this.tbPass.Size = new System.Drawing.Size(191, 27);
+            this.tbPass.Size = new System.Drawing.Size(144, 23);
             this.tbPass.TabIndex = 3;
             // 
             // btnLogin
             // 
             this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogin.Location = new System.Drawing.Point(129, 174);
+            this.btnLogin.Location = new System.Drawing.Point(97, 130);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(99, 33);
+            this.btnLogin.Size = new System.Drawing.Size(74, 25);
             this.btnLogin.TabIndex = 4;
             this.btnLogin.Text = "登录";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -87,9 +92,10 @@
             // btnExit
             // 
             this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExit.Location = new System.Drawing.Point(245, 173);
+            this.btnExit.Location = new System.Drawing.Point(184, 130);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(99, 33);
+            this.btnExit.Size = new System.Drawing.Size(74, 25);
             this.btnExit.TabIndex = 5;
             this.btnExit.Text = "退出";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -97,21 +103,22 @@
             // 
             // Form_Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(408, 225);
+            this.ClientSize = new System.Drawing.Size(306, 169);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.tbPass);
             this.Controls.Add(this.tbUser);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form_Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "林地展示地理信息系统 - 登录";
+            this.Text = "深圳市罗湖区林地展示平台V1.0 - 登录";
             this.ResumeLayout(false);
             this.PerformLayout();
 
